dd5e16f16b66293de437462b99a78fc69e86e3a7
AllyReyes <89092833+AllyReyes@users.noreply.github.com>
2021-10-07 18:18:48 -0400

    adding files

---
+++ mySuperAmazingProject/src/main/java/com/tts/mySuperAmazingProject/MySuperAmazingProjectApplication.java
@@ -0,0 +1,13 @@
+package com.tts.mySuperAmazingProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySuperAmazingProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySuperAmazingProjectApplication.class, args);
+	}
+
+}
---
+++ mySuperAmazingProject/src/test/java/com/tts/mySuperAmazingProject/MySuperAmazingProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tts.mySuperAmazingProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySuperAmazingProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
